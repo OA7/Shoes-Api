34cf7789d4b7cd052d3cd6caa9520dd7e74a1e57
Osong Agberndifor <agberndifor7@gmail.com>
2021-01-06 12:35:06 +0100

    Add controller and routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,6 @@ class UsersController < ApplicationController
   private 
 
   def user_params
-    params.permit(:username, :password,  :password_confirmation)
+    params.permit(:username, :password)
   end
 end
